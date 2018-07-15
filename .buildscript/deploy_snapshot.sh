f27266306c90a58ce48dcb5e291dab5055820b52
Ash Davies <ashdavies@users.noreply.github.com>
2018-07-15 14:08:07 +0200

    Use repository slug for validation of snapshot deployment branch

--- .buildscript/deploy_snapshot.sh
+++ .buildscript/deploy_snapshot.sh
@@ -5,17 +5,17 @@
 # Adapted from https://coderwall.com/p/9b_lfq and
 # http://benlimmer.com/2013/12/26/automatically-publish-javadoc-to-gh-pages-with-travis-ci/
 
-URL="https://github.com/ashdavies/rx-tasks"
+SLUG="ashdavies/rx-tasks"
 BRANCH="master"
 
 set -e
 
-if [ "$CIRCLE_REPOSITORY_URL" != "$URL" ]; then
-  echo "Skipping snapshot deployment: wrong repository. Expected '$URL' but was '$CIRCLE_REPOSITORY_URL'."
-elif [ "$CIRCLE_PULL_REQUEST" != "false" ]; then
-  echo "Skipping snapshot deployment: was pull request."
+if [ "$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME" != "$SLUG" ]; then
+  echo "Skipping snapshot deployment: wrong repository. Expected '$SLUG' but was '$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME'."
 elif [ "$CIRCLE_BRANCH" != "$BRANCH" ]; then
   echo "Skipping snapshot deployment: wrong branch. Expected '$BRANCH' but was '$CIRCLE_BRANCH'."
+elif [ ! -z "$CIRCLE_PULL_REQUEST" ]; then
+  echo "Skipping snapshot deployment: was pull request."
 else
   echo "Deploying snapshot..."
   ./gradlew uploadArchives
